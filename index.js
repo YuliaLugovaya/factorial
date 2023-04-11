27262e79684fddeeabd8e26d9c0e361c559e34e3
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-04-11 13:51:37 +0300

    feat: add solutions

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 // Итерационный подсчет факториала
-function factIterat(num){
-
+function factIterat(num) {
+  if (num > 0) {
+    let arr = [];
+    for (let i = 1; i <= num; i++) {
+      arr.push(i);
+    }
+    return arr.reduce((acc, el) => acc * el);
+  } else {
+    return 'Значение аргумента меньше или равно нулю';
+  }
 }
+console.log(factIterat(4))
 
 // Рекурсивный расчет факториала
-function factRec(num){
- 
+function factRec(num) {
+  if (num > 0) {
+    if (num === 1) {
+      return 1;
+    }
+    return num * factRec(num - 1);
+  } else {
+    return 'Значение аргумента меньше или равно нулю';
+  }
 }
+console.log(factRec(3))
